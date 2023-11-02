e6d5899ac930cf9d1ee75751b1b84a83a5c7721a
Prashant Pandey <prashant.pan2205@gmail.com>
2023-11-02 15:01:55 +0530

    1_Inception

--- 1_Inception/react.js
+++ 1_Inception/react.js
@@ -1,11 +1,11 @@
 const parent = React.createElement("div", { id: "parent" }, [ 
     React.createElement("div", { id: "child" }, [ 
         React.createElement("h1", 0, "I'm an h1 tag"), 
-        React.createElement("h2", {}, "I'm an h2 tag"), 
+        React.createElement("h2", {}, "I'm an h2 tag") 
     ]),
 
     React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", ),{}, "I'm an h1 tag"), 
+        React.createElement("h1", {}, "I'm an h1 tag"), 
         React.createElement("h2", {}, "I'm an h2 tag"),]) ,
     
 
